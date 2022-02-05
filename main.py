99d6dacb45e36008c2ae2ed5d9bd5d1b6aef3b58
Vov4ik <vova_karasoff@mail.ru>
2022-02-05 20:49:34 +0300

    фывфвы

--- main.py
+++ main.py
@@ -10,27 +10,39 @@ SCREEN_SIZE = [600, 450]
 
 
 class Example(QWidget):
-    def init(self):
-        super().init()
+    def __init__(self):
+        super().__init__()
+        self.sizex = 0.05
+        self.sizey = 0.05
+        self.corx = 37.622092
+        self.cory = 55.7536308
+        self.map_request = ''
+        self.response = None
         self.getImage()
         self.initUI()
 
 
     def getImage(self):
-        self.map_request = "http://static-maps.yandex.ru/1.x/?ll=37.622092,55.7536308&spn=0.05,0.05&l=map"
-        response = requests.get(self.map_request)
+        lon = str(self.sizex)
+        lat = str(self.sizey)
+        delta = str(self.corx)
+        delta2 = str(self.cory)
+        self.map_request = "http://static-maps.yandex.ru/1.x/?ll=" + delta + "," + delta2 + "&spn=" + lon + "," + lat +\
+                           "&l=map"
+        self.response = requests.get(self.map_request)
 
-        if not response:
+        if not self.response:
             print("Ошибка выполнения запроса:")
             print(self.map_request)
-            print("Http статус:", response.status_code, "(", response.reason, ")")
+            print("Http статус:", self.response.status_code, "(", self.response.reason, ")")
             sys.exit(1)
 
         self.map_file = "map.png"
         with open(self.map_file, "wb") as file:
-            file.write(response.content)
+            file.write(self.response.content)
 
     def initUI(self):
+        print(80)
         self.setGeometry(100, 100, *SCREEN_SIZE)
         self.setWindowTitle('Отображение карты')
 
@@ -39,6 +51,30 @@ class Example(QWidget):
         self.image.move(0, 0)
         self.image.resize(600, 450)
         self.image.setPixmap(self.pixmap)
+
+    def keyPressEvent(self, event):
+        if event.key() == Qt.Key_PageUp:
+            if self.sizex < 1:
+                self.sizex += 0.01
+                self.sizey += 0.01
+        if event.key() == Qt.Key_PageDown:
+            if self.sizex > 0.01:
+                self.sizex -= 0.01
+                self.sizey -= 0.01
+        if event.key() == Qt.Key_Left:
+            if self.corx < 50:
+                self.corx += 0.01
+        if event.key() == Qt.Key_Right:
+            if self.corx > 20.0:
+                self.corx -= 0.01
+        if event.key() == Qt.Key_Up:
+            if self.cory < 70:
+                self.cory += 0.01
+        if event.key() == Qt.Key_Down:
+            if self.cory > 40:
+                self.cory -= 0.01
+        self.getImage()
+
 
     def closeEvent(self, event):
         os.remove(self.map_file)
